82324152d83a3c52d72aee7ba83b1aaebc0afa69
Evtim Georgiev Lyonov <elyon@softserveinc.com>
2022-02-07 16:32:06 +0200

    Fixing the position of the footer

--- src/components/MainContainer/MainContainer.tsx
+++ src/components/MainContainer/MainContainer.tsx
@@ -3,6 +3,6 @@ interface Props {
 }
 export const MainContainer = ({ children }: Props) => {
   return (
-    <main className='flex flex-col min-h-[calc(100vh-3.5rem)]'>{children}</main>
+    <main className='flex flex-col min-h-[calc(100vh-5.2rem)]'>{children}</main>
   );
 };
