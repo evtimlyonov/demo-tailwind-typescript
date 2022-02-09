b27db9ad12a96aa19226982132bbfe7b89885fe4
Evtim Georgiev Lyonov <elyon@softserveinc.com>
2022-02-09 10:50:11 +0200

    Desctop version packages overflow fixed

--- src/components/MainContainer/MainContainer.tsx
+++ src/components/MainContainer/MainContainer.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 export const MainContainer = ({ children }: Props) => {
   return (
-    <main className='flex flex-col min-h-[calc(100vh-5.2rem)] mt-16'>
+    <main className='flex flex-col min-h-[calc(100vh-5.2rem)] mt-16 overflow-hidden'>
       {children}
     </main>
   );
